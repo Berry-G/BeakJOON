1102b474ce7cbd714721558bb5b62f517e590520
Berry-G <chae3920@kakao.com>
2023-03-02 04:45:00 +0900

    1008

---
+++ BaekJOON/src/kr/kyc/AdivB.java
@@ -0,0 +1,15 @@
+package kr.kyc;
+
+import java.util.Scanner;
+
+public class AdivB {
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		double a, b;
+		a = scanner.nextDouble();
+		b = scanner.nextDouble();
+		System.out.println(a/b);
+		scanner.close();
+	}
+}
