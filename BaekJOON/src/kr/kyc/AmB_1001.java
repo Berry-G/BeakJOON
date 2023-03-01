b1a6ea2ecc7bcd63fa871e0ddc88359365e326d6
Berry-G <chae3920@kakao.com>
2023-03-02 02:07:03 +0900

    1001

---
+++ BaekJOON/src/kr/kyc/AmB_1001.java
@@ -0,0 +1,15 @@
+package kr.kyc;
+
+import java.util.Scanner;
+
+public class AmB_1001 {
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a-b);
+		scanner.close();
+	}
+}
