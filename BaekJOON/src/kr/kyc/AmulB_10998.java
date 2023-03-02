4d8507188061d329bc6fd7efb619d296787af0ba
Berry-G <chae3920@kakao.com>
2023-03-02 22:38:39 +0900

    f

---
+++ BaekJOON/src/kr/kyc/AmulB_10998.java
@@ -0,0 +1,15 @@
+package kr.kyc;
+
+import java.util.Scanner;
+
+public class AmulB_10998 {
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a*b);
+		scanner.close();
+	}
+}
