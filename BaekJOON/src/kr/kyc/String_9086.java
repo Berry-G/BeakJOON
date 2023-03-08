99489a7f5cfa15f44d5fb8091ce1603fbb9eec1c
Berry-G <chae3920@kakao.com>
2023-03-09 02:12:43 +0900

    9086 정답

--- BaekJOON/src/kr/kyc/String_9086.java
+++ BaekJOON/src/kr/kyc/String_9086.java
@@ -3,19 +3,18 @@ package kr.kyc;
 import java.util.Scanner;
 
 public class String_9086 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int size = scanner.nextInt();
-		String str = scanner.next();
-		System.out.println(str.substring(size));
+		for (int i = 0; i < size; i++) {
+			String str = scanner.next();
+			// 첫글자 추출
+			String fChar = str.substring(0, 1);
+			// 마지막글자 추출
+			String lChar = str.substring(str.length() - 1, str.length());
+			String result = fChar + lChar;
+			System.out.println(result);
+		}
 		scanner.close();
 	}
 }
-
-/*
-3
-ACDKJFOWIEGHE
-O
-AB
-*/
