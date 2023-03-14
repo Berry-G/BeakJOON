5510c475bee565403236a5ea3be3179f6811580b
Berry-G <chae3920@kakao.com>
2023-03-14 22:21:47 +0900

    230314 step2

--- BaekJOON/src/kr/kyc/step2/Test_results_9498.java
+++ BaekJOON/src/kr/kyc/step2/Test_results_9498.java
@@ -5,8 +5,24 @@ import java.util.Scanner;
 public class Test_results_9498 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
-		
-		
+
+		int score = scan.nextInt();
+		String result;
+		if (score >= 90)
+			result = "A";
+		else if (score >= 80 && score < 90)
+			result = "B";
+		else if (score >= 70 && score < 80)
+			result = "C";
+		else if (score >= 60 && score < 70)
+			result = "D";
+		else
+			result = "F";
+		System.out.println(result);
 	}
 }
+
+/*
+ * 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나 머지 점수는 F를
+ * 출력하는 프로그램을 작성하시오.
+ */
