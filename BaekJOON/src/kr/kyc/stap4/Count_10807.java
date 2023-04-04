14ac94085a9d6f3b38d40a4b05b0759431de9804
Berry-G <chae3920@kakao.com>
2023-04-05 05:25:21 +0900

    20230405 solved

--- BaekJOON/src/kr/kyc/stap4/Count_10807.java
+++ BaekJOON/src/kr/kyc/stap4/Count_10807.java
@@ -6,7 +6,7 @@ public class Count_10807 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-		int x[] = new int[];
+		int []x = new int[n];
 		int cnt=0;
 		for(int i=0; i<n; i++)
 		{
