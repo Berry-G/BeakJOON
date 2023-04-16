a930484e813108fa23252b8a0d283ddeb7e407f1
Berry-G <chae3920@kakao.com>
2023-04-16 16:29:41 +0900

    230416

--- BaekJOON/src/kr/kyc/step4/Goalin_10810.java
+++ BaekJOON/src/kr/kyc/step4/Goalin_10810.java
@@ -1,8 +1,15 @@
 package kr.kyc.step4;
 
+import java.util.Scanner;
+
 public class Goalin_10810 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		
+		
 		
 	}
 }
