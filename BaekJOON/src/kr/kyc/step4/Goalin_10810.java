0f73b5b1360400f5c53e42403e8267f98be54d45
Berry-G <chae3920@kakao.com>
2023-04-19 06:13:46 +0900

    230419 sovled & memo

--- BaekJOON/src/kr/kyc/step4/Goalin_10810.java
+++ BaekJOON/src/kr/kyc/step4/Goalin_10810.java
@@ -1,5 +1,6 @@
 package kr.kyc.step4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Goalin_10810 {
@@ -8,15 +9,24 @@ public class Goalin_10810 {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		int m = scanner.nextInt();
-		int [][]arr = new int[n][m];
-		for(int i=0; i<n; i++)
+		int []arr = new int[n+1];
+		for(int i=0; i<=n; i++)
 		{
-			
+			arr[i]=0;
 		}
-		
-		
-		
-		
-		
+		for(int j=0; j<m; j++)
+		{
+			int a = scanner.nextInt();	//바구니 시작번호
+			int b = scanner.nextInt();	//바구니 끝번호
+			int c = scanner.nextInt();	//공 넣기
+			for(int i = a; i<=b; i++)
+			{
+				arr[i] = c;
+			}
+		}
+		for(int i=1; i<=n; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
 	}
 }
