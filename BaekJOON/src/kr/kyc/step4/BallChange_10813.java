7c6720135db25f1228fe2ecd7da251091f64c671
Berry-G <chae3920@kakao.com>
2023-04-25 23:45:47 +0900

    230425 solved

--- BaekJOON/src/kr/kyc/step4/BallChange_10813.java
+++ BaekJOON/src/kr/kyc/step4/BallChange_10813.java
@@ -8,20 +8,23 @@ public class BallChange_10813 {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();		//N개 바구니
 		int m = scanner.nextInt();		//M번 바꿈
-		int []arr = new int[n+1];		//첫 바구니는 비어있음
+		int []arr = new int[n+1];		//첫 바구니는 비어있고 결과값 출력X
+		
+		//배열 초기값 설정
 		for(int i=0; i<=n; i++)
 		{
-			arr[i]=0;
-		} 
+			arr[i]=i;
+		}
+		
+		//M번 바꿈
 		for(int j=0; j<m; j++)
 		{
-			int a = scanner.nextInt();	//바구니 시작번호
-			int b = scanner.nextInt();	//바구니 끝번호
-			int c = scanner.nextInt();	//공 넣기
-			for(int i = a; i<=b; i++)
-			{
-				arr[i] = c;
-			}
+			int a, b, temp;
+			a = scanner.nextInt();	//바구니 시작번호
+			b = scanner.nextInt();	//바구니 끝번호
+			temp = arr[a];
+			arr[a] = arr[b];
+			arr[b] = temp;
 		}
 		for(int i=1; i<=n; i++)
 		{
