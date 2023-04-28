8ab860951f3dd62dc00ff9084a5cc5ecaa5a8c32
Berry-G <chae3920@kakao.com>
2023-04-28 23:20:53 +0900

    230428 solved

--- BaekJOON/src/kr/kyc/step4/NoReport_5597.java
+++ BaekJOON/src/kr/kyc/step4/NoReport_5597.java
@@ -3,29 +3,27 @@ package kr.kyc.step4;
 import java.util.Scanner;
 
 public class NoReport_5597 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int[] arr = new int[30];
-		int[] result = new int[2];
-		for(int i=0; i<28; i++)
-		{
-			arr[i] = scanner.nextInt();
-		}
-		for(int i=0; i<28; i++)
-		{
-			int cnt = 0;
-			for(int j=0; j<30; j++)
-			{
-				if(arr[i] == j)
-				{
-					cnt++;
-				}
-				if(cnt > 0)
-				{
-					result[cnt] = j;
-				}
-
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[31];
+        int[] result = {0, 0};
+        int count=0;
+        for(int i=0; i<28; i++)
+        {
+            int num = scanner.nextInt();
+            arr[num] = 1;
+        }
+        for(int i=1; i<=30; i++)
+        {
+            if(arr[i] == 0)
+            {
+                result[count] = i;
+                count++;
+            }
+        }
+        for(int i=0; i<2; i++)
+        {
+            System.out.println(result[i]);
+        }
+    }
 }
