bb276505e59acb312a461a9f7d4b279f1181c51a
Berry-G <chae3920@kakao.com>
2023-05-05 04:14:26 +0900

    access token expire?

---
+++ BaekJOON/src/kr/kyc/step4/AVG_1546.java
@@ -0,0 +1,12 @@
+package kr.kyc.step4;
+
+import java.util.Scanner;
+
+public class AVG_1546 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();		//N개 과목
+		//int []score = new int[n+1];
+		for(int i = 0; ;);
+	}
+}
