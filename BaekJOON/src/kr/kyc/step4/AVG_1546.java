2b27e3a95e1127f2aca2136291fd107aa15bf6af
Berry-G <chae3920@kakao.com>
2023-05-05 07:19:34 +0900

    230505 solved

--- BaekJOON/src/kr/kyc/step4/AVG_1546.java
+++ BaekJOON/src/kr/kyc/step4/AVG_1546.java
@@ -6,7 +6,19 @@ public class AVG_1546 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();		//N개 과목
-		//int []score = new int[n+1];
-		for(int i = 0; ;);
+		double []score = new double[n];
+		double max=0, sum=0;
+		for(int i = 0; i < n; i++)
+		{
+			score[i] = scanner.nextInt();
+			max = Math.max(max, score[i]);
+		}
+		//예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+		for(int i = 0; i < n; i++)
+		{
+			score[i] = score[i] / max * 100;
+			sum += score[i];
+		}
+		System.out.println(sum/n);
 	}
 }
