3de13ebbfb52f35ba982985ce50401ed81d76f6b
Berry-G <chae3920@kakao.com>
2023-05-07 23:18:05 +0900

    230507_solved

---
+++ BaekJOON/src/kr/kyc/step5/ASCII_11654.java
@@ -0,0 +1,12 @@
+package kr.kyc.step5;
+
+import java.util.Scanner;
+
+public class ASCII_11654 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char x = scanner.next().charAt(0);
+		System.out.println((int)x);
+		
+	}
+}
