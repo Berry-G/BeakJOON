4a5a5937fc2788e4819abbbe0871f91a72924098
Berry-G <chae3920@kakao.com>
2023-05-10 06:00:30 +0900

    230510_solved

--- BaekJOON/src/kr/kyc/step5/StringLength_2743.java
+++ BaekJOON/src/kr/kyc/step5/StringLength_2743.java
@@ -2,13 +2,10 @@ package kr.kyc.step5;
 
 import java.util.Scanner;
 
-public class CharString_27866 {
+public class StringLength_2743 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String S = scanner.nextLine();
-		char[] SChar = S.toCharArray();		//S를 char 배열로 변환
-		
-		int i = scanner.nextInt();
-		System.out.println(SChar[i-1]);
+		System.out.println(S.length());
 	}
 }
