e89f32ee18f6646ce4cd85f02e55c5409e025fa5
Berry-G <chae3920@kakao.com>
2023-05-27 23:42:02 +0900

    230527 solving

--- BaekJOON/src/kr/kyc/step5/SamePrint_11718.java
+++ BaekJOON/src/kr/kyc/step5/SamePrint_11718.java
@@ -2,26 +2,23 @@ package kr.kyc.step5;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class SamePrint_11718 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String[] sentence;
-		 while ((sentence = br.readLine()) != null) {
-	            if (sentence.length() == 0) {
-	                break;
-	            }
-	            StringTokenizer st = new StringTokenizer(sentence);
-	            int A = Integer.parseInt(st.nextToken());
-	            int B = Integer.parseInt(st.nextToken());
-	            String result = Integer.toString(A + B) + "\n";
-	            bw.write(result);
-	        }
-	        bw.close();
-	    }
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String sentence;
+		while ((sentence = br.readLine()) != null) {
+			if (sentence.length() == 0) {
+				break;
+			}
+			StringTokenizer st = new StringTokenizer(sentence);
+			bw.write(sentence);
+		}
+		bw.close();
 	}
 }
