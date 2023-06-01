205a7165348bbbbf9f1218d041874d71b1d1d2d1
Berry-G <chae3920@kakao.com>
2023-06-01 23:39:40 +0900

    230601 solved

---
+++ BaekJOON/src/kr/kyc/step6/Sprout_25083.java
@@ -0,0 +1,15 @@
+package kr.kyc.step6;
+
+public class Sprout_25083 {
+	public static void main(String[] args){
+		String A = "         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |";
+		System.out.println(A);
+				
+	}
+}
+
