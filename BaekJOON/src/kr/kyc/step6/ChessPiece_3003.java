fac2ca915ba7ab2ed67f271566fc19654ccde735
Berry-G <chae3920@kakao.com>
2023-06-05 23:44:43 +0900

    230605 solving

---
+++ BaekJOON/src/kr/kyc/step6/ChessPiece_3003.java
@@ -0,0 +1,13 @@
+package kr.kyc.step6;
+
+import java.util.Scanner;
+
+public class ChessPiece_3003 {
+	public static void main(String[] args) {
+		//체스는 총 16개의 피스를 사용하며, 킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개로 구성되어 있다.
+		//킹 퀸 룩 비숍 나이트 폰
+
+		Scanner scanner = new Scanner(System.in);
+		int[] piece = new int[6];
+	}
+}
