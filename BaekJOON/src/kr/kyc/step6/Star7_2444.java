98fef224b393a3b19549ae5a54275a702b9531e5
Berry-G <chae3920@kakao.com>
2023-06-07 23:53:33 +0900

    230607 solving

---
+++ BaekJOON/src/kr/kyc/step6/Star7_2444.java
@@ -0,0 +1,18 @@
+package kr.kyc.step6;
+
+import java.util.Scanner;
+
+public class Star7_2444 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt(); // N * 2 - 1
+		
+		//첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
+		for(int i=0; i<(2 * n - 1); n++)
+		{
+			
+		}
+		
+	}
+
+}
