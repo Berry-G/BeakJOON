ba1c8ae2fdae6acef0d789284288c65d09d203e4
Berry-G <chae3920@kakao.com>
2023-06-09 23:55:56 +0900

    230609 solving

--- BaekJOON/src/kr/kyc/step6/Star7_2444.java
+++ BaekJOON/src/kr/kyc/step6/Star7_2444.java
@@ -8,9 +8,13 @@ public class Star7_2444 {
 		int n = scanner.nextInt(); // N * 2 - 1
 		
 		//첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
-		for(int i=0; i<(2 * n - 1); n++)
+		for(int i=0; i<n-1; i++)
 		{
-			
+			for(int j=0; j<n-i-1; j++)
+			{
+				System.out.print("*");			
+			}
+			System.out.println();
 		}
 		
 	}
