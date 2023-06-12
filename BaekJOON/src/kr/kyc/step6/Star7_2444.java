ec3343b985f298784f47460f4152ba9c4f50c075
Berry-G <chae3920@kakao.com>
2023-06-12 23:22:17 +0900

    230612 solving

--- BaekJOON/src/kr/kyc/step6/Star7_2444.java
+++ BaekJOON/src/kr/kyc/step6/Star7_2444.java
@@ -10,13 +10,32 @@ public class Star7_2444 {
 		//첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
 		for(int i=0; i<n-1; i++)
 		{
-			for(int j=0; j<n-i-1; j++)
+			for(int j=i; j<n-1; j++)
 			{
-				System.out.print("*");			
+				System.out.print("O");		//space
+			}
+			//for(int )
+			for(int j=2; j<(i+1)*4; j=j+2)
+			{
+				System.out.print("*");
 			}
 			System.out.println();
 		}
-		
+		System.out.println();
+		for(int i=0; i<n-1; i++)
+		{
+			for(int j=0; j<i+1; j++)
+			{
+				System.out.print("O");		//space
+			}
+
+			for(int j=i+15; j<(n)*2; j=j+2)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+					
 	}
 
 }
