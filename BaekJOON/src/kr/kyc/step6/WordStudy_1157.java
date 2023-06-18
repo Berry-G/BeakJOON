63e24ee1861ca4da8503f181be94111652843d94
Berry-G <chae3920@kakao.com>
2023-06-19 04:39:00 +0900

    230619 solving

--- BaekJOON/src/kr/kyc/step6/WordStudy_1157.java
+++ BaekJOON/src/kr/kyc/step6/WordStudy_1157.java
@@ -6,7 +6,12 @@ public class WordStudy_1157 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String word = scanner.nextLine();
-		
+		char[] wordArray = word.toCharArray();		//word를 char 배열로 변환
+		for(int i=0; i<word.length(); i++)
+		{
+			System.out.println(wordArray[i]);
+			
+		}
 	}
 
 }
