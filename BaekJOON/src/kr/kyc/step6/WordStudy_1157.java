641e414a80c99a12db4d12c54df7090e992d3c10
Berry-G <chae3920@kakao.com>
2023-06-18 23:57:20 +0900

    230618 solving

---
+++ BaekJOON/src/kr/kyc/step6/WordStudy_1157.java
@@ -0,0 +1,12 @@
+package kr.kyc.step6;
+
+import java.util.Scanner;
+
+public class WordStudy_1157 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String word = scanner.nextLine();
+		
+	}
+
+}
