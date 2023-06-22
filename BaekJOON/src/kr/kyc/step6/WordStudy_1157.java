52f42f69f8f25e78aa78c44b945c7f8dd8ee292f
Berry-G <chae3920@kakao.com>
2023-06-22 23:51:59 +0900

    230622 solving

--- BaekJOON/src/kr/kyc/step6/WordStudy_1157.java
+++ BaekJOON/src/kr/kyc/step6/WordStudy_1157.java
@@ -1,5 +1,6 @@
 package kr.kyc.step6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class WordStudy_1157 {
@@ -7,11 +8,20 @@ public class WordStudy_1157 {
 		Scanner scanner = new Scanner(System.in);
 		String word = scanner.nextLine();
 		char[] wordArray = word.toCharArray();		//word를 char 배열로 변환
+		int[] wordCnt = new int[26];
 		for(int i=0; i<word.length(); i++)
 		{
 			System.out.println(wordArray[i]);
-			
+			for(int j=0; j<26; j++)
+			{
+				if(wordArray[i] == 'a' + j || wordArray[i] == 'A' + j)
+				{
+					wordCnt[j]++;
+					break;
+				}
+			}
 		}
+		System.out.println(Arrays.toString(wordCnt));
 	}
 
 }
