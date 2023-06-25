e762313538a6893b2db42f7b972ece591df8aefd
Berry-G <chae3920@kakao.com>
2023-06-25 23:43:07 +0900

    230625 solving

---
+++ BaekJOON/src/kr/kyc/step6/CroatianAlphabet.java
@@ -0,0 +1,16 @@
+package kr.kyc.step6;
+
+import java.util.Scanner;
+
+public class CroatianAlphabet {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String word = scanner.nextLine();
+		char[] wordArray = word.toCharArray();		//word를 char 배열로 변환
+		
+		/*
+		 * 크로아티아 알파벳
+		 * c=, c-, dz=, d-, lj
+		 */
+	}
+}
