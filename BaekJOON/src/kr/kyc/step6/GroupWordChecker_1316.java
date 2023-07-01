a00d59e9f5a4a0fdea88342a84c08af3d3bd9b06
Young-Chae KIM <69233747+Berry-G@users.noreply.github.com>
2023-07-01 20:07:48 +0900

    230701 solving

--- BaekJOON/src/kr/kyc/step6/GroupWordChecker_1316.java
+++ BaekJOON/src/kr/kyc/step6/GroupWordChecker_1316.java
@@ -6,19 +6,44 @@ public class GroupWordChecker_1316 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int cnt=0;
-		int n = scanner.nextInt();
-		for(int i = 0; i<=n; i++)
+		// int n = scanner.nextInt();
+
+		String word = scanner.nextLine();
+		char[] wordArray = word.toCharArray();
+
+		System.out.println();
+
+		//입력되는 단어는 소문자 알파벳으로만 제한된다.
+		char[] chkChar = new char[26];
+		int chkIndex = 0;
+		int escape = 0;
+		for(int i=1; i<word.length(); i++)
 		{
-			String word = scanner.nextLine();
-			char[] wordArray = word.toCharArray();		//word를 char 배열로 변환
-			
-			for(int j=0; j<word.length(); j++)
+			if(wordArray[i] != wordArray[i-1])
 			{
-				char[] saveArray = new char[26];
-				System.out.print(wordArray[j]);
+				chkChar[chkIndex] = wordArray[i-1];
+			}
+			for(int j=0; j<=chkIndex; j++)
+			{
+				// System.out.print(chkChar[j] + " + " + wordArray[i-1]);
+				// System.out.println();
+				if(chkChar[j] == wordArray[i-1])
+				{
+					System.out.println("중복문자 발견");
+					escape++;
+					break;
+				}
+			}
+			if(escape == 1)
+			{
+				System.out.println("escape");
+				break;
 			}
 		}
-		
+		if(escape == 0)
+		{
+			cnt++;
+		}
 		System.out.println(cnt);
 	}
 }
