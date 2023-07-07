73d4226c15eb29c1e6bf3b7754372d7fa16a6bd0
Young-Chae KIM <69233747+Berry-G@users.noreply.github.com>
2023-07-07 23:57:59 +0900

    20230707 solving

---
+++ BaekJOON/src/kr/kyc/step6/YourScore_25206.java
@@ -0,0 +1,27 @@
+package kr.kyc.step6;
+
+//import java.util.Arrays;
+import java.util.Scanner;
+
+public class YourScore_25206 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+        String subject = scanner.next();
+        double score = scanner.nextDouble();
+        String grade = scanner.next();
+
+        System.out.println(subject);
+        System.out.println(score);
+        System.out.println(grade);
+
+        // 전공평점은 전공과목별 (학점 × 과목평점)의 합을 학점의 총합으로 나눈 값이다.
+
+
+    }
+
+    static void gradeToDouble()
+
+}
