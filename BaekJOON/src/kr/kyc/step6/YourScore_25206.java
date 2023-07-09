938e533eaac149cf70730da4d3148b6ef9c45f58
Young-Chae KIM <69233747+Berry-G@users.noreply.github.com>
2023-07-09 18:24:54 +0900

    20230709 solved

--- BaekJOON/src/kr/kyc/step6/YourScore_25206.java
+++ BaekJOON/src/kr/kyc/step6/YourScore_25206.java
@@ -1,31 +1,33 @@
 package kr.kyc.step6;
 
-//import java.util.Arrays;
 import java.util.Scanner;
 
 public class YourScore_25206 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         final int n = 20;
-        int cnt = n;
+        // int cnt = n;    //Pass를 제외한 총 과목갯수
+        //전공평점은 전공과목별 (학점 × 과목평점)의 합을 학점의 총합으로 나눈 값이다.
+        double majorSum = 0;   //전공과목별 (학점 × 과목평점)의 합
+        double scoreSum = 0;   //학점의 합
         for(int i = 0; i<n; i++)
         {
             String subject = scanner.next();
             double score = scanner.nextDouble();
             String grade = scanner.next();
+
+            if(!grade.equals("P"))
+            {
+                majorSum = majorSum + (score * gradeToDouble(grade));
+                scoreSum = scoreSum + score;
+            }
+
+            // System.out.println(scoreSum);
         }
-        
 
-
-
-        System.out.println(subject);
-        System.out.println(score);
-        System.out.println(grade);
-        System.out.println(gradeToDouble(grade));
-        System.out.println("정답" + score*gradeToDouble(grade));
-
-        // 전공평점은 전공과목별 (학점 × 과목평점)의 합을 학점의 총합으로 나눈 값이다.
-
+        // System.out.println(majorSum);
+        // System.out.println(scoreSum);
+        System.out.println(majorSum / scoreSum);
 
     }
 
@@ -40,7 +42,7 @@ public class YourScore_25206 {
         else if(grade.equals("D+")) return 1.5;
         else if(grade.equals("D0")) return 1.0;
         else if(grade.equals("F")) return 0.0;
-        else    return 10;
+        else    return 0;
     }
 
 }
